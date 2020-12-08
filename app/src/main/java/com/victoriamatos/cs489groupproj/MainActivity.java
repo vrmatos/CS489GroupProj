c817c4fde210b0f9a58ff598f51ae12513cdd226
juliahart <jahart@loyola.edu>
2020-12-07 20:43:49 -0500

    ad code commented out

--- app/src/main/java/com/victoriamatos/cs489groupproj/MainActivity.java
+++ app/src/main/java/com/victoriamatos/cs489groupproj/MainActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.AdSize;
+//import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+      //  AdView adView = new AdView( this );
+        //adView.setAdSize( AdSize.SMART_BANNER );
+       // String adUnitId ="ca-app-pub-3940256099942544/6300978111";
+        //adView.setAdUnitId( adUnitId );
+
+        //AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+        // define t
+        // arget area
+        //adRequestBuilder.addKeyword("workout").addKeyword("fitness");
+        //adRequestBuilder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
+        //AdRequest adRequest = adRequestBuilder.build();
+
+      //  LinearLayout adLayout = (LinearLayout) findViewById( R.id.ad_view );
+        //adLayout.addView( adView );
+
+        //adView.loadAd( adRequest );
     }
 
     public void playSpelling(View v){
